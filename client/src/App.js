c3509f41326bd1b818c03143016eb1da27b1b5ec
Sajib <contact2sajib@gmail.com>
2022-10-28 17:20:53 +0600

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import Home from './Pages/Home'
 
 const App = () => {
+
+  // const user = useSelector((state)=> state?.authReducer?.authData)
+
   return (
-    <div className="App">
+    <div style={{margin: '10px'}}>
       Empty React App
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" exact element={<Home />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }

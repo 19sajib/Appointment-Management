a1c4db206718a3c41db913b75aff9849c57743c4
Sajib <contact2sajib@gmail.com>
2022-11-02 09:19:36 +0600

    api middleware

--- server/Routes/AppointmentRoute.js
+++ server/Routes/AppointmentRoute.js
@@ -1,16 +1,17 @@
 const express = require('express')
 
 const { getAppointment, getAppointData, allAppointment, cancelAppointment, getDate, getSlot, singleAppointment, rescheduleAppointment } = require('../Controllers/Appointment')
+import { verifyToken, verifyTokenAndAdmin, verifyTokenAndAuthorization } from '../Middleware/verifyToken.js'
 
 const router = express.Router()
 
-router.post('/date', getDate)
-router.post('/slot', getSlot)
-router.get('/all', allAppointment)
-router.get('/user/:id', getAppointData)
-router.post('/booking', getAppointment)
-router.get('/single/:id', singleAppointment)
-router.get('/cancel/:id', cancelAppointment)
-router.post('/reschedule/:id', rescheduleAppointment)
+router.post('/date', verifyToken, getDate)
+router.post('/slot', verifyToken, getSlot)
+router.get('/all', verifyTokenAndAdmin, allAppointment)
+router.get('/user/:id', verifyTokenAndAuthorization, getAppointData)
+router.post('/booking', verifyToken, getAppointment)
+router.get('/single/:id', verifyToken, singleAppointment)
+router.get('/cancel/:id', verifyToken, cancelAppointment)
+router.post('/reschedule/:id', verifyToken, rescheduleAppointment)
 
 module.exports = router
